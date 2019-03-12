58a4dfe71b8ab3e20967869134a3ab5acdd75f66
Matěj Kneifl <matej.kneifl@seznam.cz>
2019-03-12 11:53:40 +0100

    Update index.php
    BREAAAAAAAAAK

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         $lodHP  = 200;
         $strela = rand(0,5);
 
-      echo "0 Loď je bez nákladu a na Apalu je 100 nákladu. <br>";
+      echo "0 Nákladu loď je bez nákladu a na Apalu je 100 nákladu. <br>";
 
   while ($botak < 100) {
     $botak = $botak + 10;
@@ -21,7 +21,7 @@
     echo $botak . " nákladu je na Botaku. zbytek nákladu na Apalu : " . $apal . ".<br> A zbylé HP lodi je : " . $lodHP . ".<br>";
     if ($lodHP == 0) {
       echo "Loď zničena!! Náklad nebude dále převozován!";
-
+      break;
     }
 
     if ($apal == 0) {
