4ed9321674a9f2ed73b636c0518eca7fbafc01ad
Matěj Kneifl <matej.kneifl@seznam.cz>
2019-03-26 14:25:52 +0100

    Update index.php
    pipi

--- index.php
+++ index.php
@@ -11,29 +11,30 @@
         $botak  = 0;
         $lodHP  = 200;
 
-         echo "0 Nákladu loď je bez nákladu a na Apalu je 100 nákladu. <br>";
+        echo "0 Nákladu loď je bez nákladu a na Apalu je 100 nákladu. <br>";
 
-        while ($botak < 100) {
-          $apal  = $apal - 10;
-          for ($i=0; $i < 5 ; $i++) {
-            $strela= rand(0,7);
-            $lodHP = $lodHP - $strela;
-          }
-          if ($lodHP <= 0) {
-            echo "Loď je zničena, nebude dál převážet!";
-            break;
-          }
-            $botak = $botak + 10;
+         do {
+           $apal  = $apal - 10;
+           echo "Na Apalu je: " . $apal . " nákladu. ";
+           for ($i=0; $i < 5 ; $i++) {
+             $strela= rand(0,10);
+             $lodHP = $lodHP - $strela;
+               }
+               if ($lodHP <= 0) {
+                     echo "<br>Loď je zničena, nebude dál převážet!";
+                      break;
+                }
+              $botak = $botak + 10;
+              echo $botak . " nákladu je na Botaku.";
 
-          echo $botak . " nákladu je na Botaku. zbytek nákladu na Apalu : " . $apal . ".<br> A zbylé HP lodi je : " . $lodHP . ".<br>";
+              echo  "<br> A zbylé HP lodi je : " . $lodHP . "<br>";
 
-
-
-        if ($apal == 0) {
-          echo "Na Apalu nezbývá žádný náklad.";
-        }
-
-      }
+               if ($apal == 0) {
+                    echo "Na Apalu nezbývá žádný náklad.";
+               }
+              }
+        while ($botak < 100)
      ?>
+     <?php echo "<br>Na botaku zbylo: " . $botak; ?>
   </body>
 </html>
