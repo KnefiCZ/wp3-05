5082a00b5151e3f7b5437e11cf036083df4954a1
Matěj Kneifl <matej.kneifl@seznam.cz>
2019-03-26 13:05:48 +0100

    Update index.php
    ooopoopoo

--- index.php
+++ index.php
@@ -7,24 +7,27 @@
   <body>
     <?php
 
-        $apal  = 100;
-        $botak = 0;
+        $apal   = 100;
+        $botak  = 0;
         $lodHP  = 200;
-        $strela = rand(0,5);
 
          echo "0 Nákladu loď je bez nákladu a na Apalu je 100 nákladu. <br>";
 
         while ($botak < 100) {
-          $botak = $botak + 10;
-          $apal = $apal - 10;
-          $lodHP =  $lodHP - 5*$strela;
+          $apal  = $apal - 10;
+          for ($i=0; $i < 5 ; $i++) {
+            $strela= rand(0,7);
+            $lodHP = $lodHP - $strela;
+          }
+          if ($lodHP <= 0) {
+            echo "Loď je zničena, nebude dál převážet!";
+            break;
+          }
+            $botak = $botak + 10;
 
           echo $botak . " nákladu je na Botaku. zbytek nákladu na Apalu : " . $apal . ".<br> A zbylé HP lodi je : " . $lodHP . ".<br>";
 
-        if ($lodHP == 0) {
-          echo "Loď zničena!! Náklad nebude dále převozován!";
-        break;
-        }
+
 
         if ($apal == 0) {
           echo "Na Apalu nezbývá žádný náklad.";
